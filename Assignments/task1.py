134d99f65313b6c1829d58d9a646159fbbf7135f
RajPatel7115 <rajpateliil7115@gmai.com>
2025-12-23 13:28:24 +0530

    first

---
+++ Assignments/task1.py
@@ -0,0 +1,23 @@
+# print("hello",end="")
+
+# a=10    #data members
+# b=30
+# print("Value of A : ",a)
+# print("Value of A : ",b)
+# print("Addition : ",a+b)
+# print("Addition : ",a-b)
+# print("Addition : ",a*b)
+# print("Addition : ",a/b)
+
+
+# a = int(input("Enter Number 1: "))  #type casting
+# b = int(input("Enter Number 2: "))
+
+# print(type(a))
+# print(type(b))
+# print("Value of A : ",a)
+# print("Value of A : ",b)
+# print("Addition : ",a+b)
+# print("Addition : ",a-b)
+# print("Addition : ",a*b)
+# print("Addition : ",a/b)
---
+++ Assignments/task2.py
@@ -0,0 +1,52 @@
+#swapping 
+
+# a = int(input("Enter Number 1 : "))
+# b = int(input("Enter Number 2 : "))
+
+#a=56 b=32
+#using third variable
+
+# temp=a  #temp=56 a=blank
+# a=b     #a=32   b=blank
+# b=temp  #b=56   temp=blank
+
+
+# a = a+b 
+# b = a-b
+# a = a-b
+
+
+# a,b = b,a
+
+# print("After swapping A : ",a)
+# print("After swapping B : ",b)
+
+
+
+# conversions
+
+# year = int(input("Enter Year : "))
+
+# print("Days are : ",year*365)
+
+
+
+#week - days
+
+#year - month
+
+#month - week
+
+#year - week
+
+#days - month
+
+
+
+
+
+
+
+
+
+#a=32   b=56
